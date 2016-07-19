f77e5860f6e8cb261e1f107ca28ddd7041bf4f09
kkdrz <drozdkonrad@gmail.com>
2016-07-20 00:35:53 +0200

    DAOimpl: Criteria replaced by CriteriaQuery

--- src/main/java/com/webshop/dao/impl/UserProfileImpl.java
+++ src/main/java/com/webshop/dao/impl/UserProfileImpl.java
@@ -2,18 +2,28 @@ package com.webshop.dao.impl;
 
 import com.webshop.dao.AbstractDao;
 import com.webshop.dao.UserProfileDao;
+import com.webshop.model.User;
 import com.webshop.model.UserProfile;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 @Repository("userProfileDao")
 public class UserProfileImpl extends AbstractDao<Integer, UserProfile> implements UserProfileDao
 {
     public UserProfile findByName(String name)
     {
-        Criteria crit = createEntityCriteria();
-        crit.add(Restrictions.eq("type", name));
-        return (UserProfile) crit.uniqueResult();
+        CriteriaQuery<UserProfile> query = createEntityCriteria();
+        Root<UserProfile> root = query.from(UserProfile.class);
+
+        query.select(root)
+                .where(getCriteriaBuilder().equal(root.get("type"), name));
+
+        TypedQuery<UserProfile> tq = getEntityManager().createQuery(query);
+        return tq.getSingleResult();
     }
 }
