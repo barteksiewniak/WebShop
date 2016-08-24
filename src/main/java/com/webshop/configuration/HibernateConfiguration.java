194e1e841bb2022a7eb1b06cd48a6810cdfda0da
Kaervek <barteksiewniak@gmail.com>
2016-08-24 16:44:43 +0200

    Added JavaDoc style comments for Hibernate configuration class

--- src/main/java/com/webshop/configuration/HibernateConfiguration.java
+++ src/main/java/com/webshop/configuration/HibernateConfiguration.java
@@ -22,30 +22,53 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
+/**
+ * <h1>Hibernate Config Class</h1>
+ * Class responsible for all Hibernate configuration. Full Java configuration in the replacement of older XML.
+ *
+ * @author Bartosz Siewniak
+ * @version 1.0
+ * @since 2016-07-08
+ */
 @Configuration
 @EnableTransactionManagement
 @ComponentScan({"com.webshop.configuration"})
 @PropertySource(value = {"classpath:application.properties"})
 public class HibernateConfiguration
 {
-
     @Autowired
     private Environment environment;
 
+
+    /**
+     * FactoryBean that creates a JPA EntityManagerFactory according to JPA's standard container bootstrap contract.
+     * This is the most powerful way to set up a shared JPA EntityManagerFactory in a Spring application context.
+     *
+     * @return LocalContainerEntityManagerFactoryBean
+     */
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory()
     {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan("com.webshop.model");
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan("com.webshop.model");
 
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaProperties(hibernateProperties());
+        entityManagerFactoryBean.setJpaVendorAdapter(vendorAdapter);
+        entityManagerFactoryBean.setJpaProperties(hibernateProperties());
 
-        return em;
+        return entityManagerFactoryBean;
     }
 
+    /**
+     * A factory for connections to the physical data source that this DataSource object represents.
+     * An alternative to the DriverManager facility, a DataSource object is the preferred means of getting a connection.
+     * An object that implements the DataSource interface will typically be registered with a naming service based on
+     * the JavaTM Naming and Directory (JNDI) API.
+     *
+     * @return DataSource
+     */
     @Bean
     public DataSource dataSource()
     {
@@ -57,6 +80,12 @@ public class HibernateConfiguration
         return dataSource;
     }
 
+    /**
+     * The Properties class represents a persistent set of properties. The Properties can be saved to a stream or loaded
+     * from a stream. Each key and its corresponding value in the property list is a string.
+     *
+     * @return Properties
+     */
     private Properties hibernateProperties()
     {
         Properties properties = new Properties();
@@ -67,16 +96,31 @@ public class HibernateConfiguration
         return properties;
     }
 
+    /**
+     * This is the central interface in Spring's transaction infrastructure. Applications can use this directly, but it
+     * is not primarily meant as API: Typically, applications will work with either TransactionTemplate or declarative
+     * transaction demarcation through AOP.
+     *
+     * @param entityManagerFactory Interface used to interact with the entity manager factory for the persistence unit.
+     * @return PlatformTransactionManager
+     */
     @Bean
     @Autowired
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf)
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory)
     {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(emf);
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
 
         return transactionManager;
     }
 
+    /**
+     * Bean post-processor that automatically applies persistence exception translation to any bean marked with
+     * Spring's @Repository annotation, adding a corresponding PersistenceExceptionTranslationAdvisor to the exposed
+     * proxy (either an existing AOP proxy or a newly generated proxy that implements all of the target's interfaces).
+     *
+     * @return PersistenceExceptionTranslationPostProcessor
+     */
     @Bean
     public PersistenceExceptionTranslationPostProcessor exceptionTranslation()
     {
